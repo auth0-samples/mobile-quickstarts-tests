5e4656e916e490198d2fbafa6919c2fd6d23b3ae
Benjamin Lizardo <benjamin.lizardo@techaid.co>
2019-01-08 22:19:34 -0400

    Update page_screen_ios.py

--- page_screen_ios.py
+++ page_screen_ios.py
@@ -1,7 +1,6 @@
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.remote.mobile import Mobile
 from config import *
 
 
@@ -11,31 +10,53 @@ class PageScreen:
         self.driver = driver
 
     def log_user(self):
-        wait = WebDriverWait(self.driver.instance, 15)
-        login_button = wait.until(
+
+        login_button = WebDriverWait(self.driver.instance, 15).until(
             EC.visibility_of_element_located((
-                MobileBy.XPATH, "//android.widget.Button")))
+                MobileBy.XPATH, "//@name:login"
+            ))
+        )
         login_button.click()
-        WebDriverWait(self.driver.instance, 15)
 
         self.switch_context("WEBVIEW")
 
-        assert True
+        textbox_username = WebDriverWait(self.driver.instance, 7000).until(
+            EC.visibility_of_element_located((
+                MobileBy.XPATH, "//input[@name='username']"
+            ))
+        )
+
+        textbox_password = WebDriverWait(self.driver.instance, 2).until(
+            EC.visibility_of_element_located((
+                MobileBy.XPATH, "//input[@name='password']"
+            ))
+        )
+
+        button_login = WebDriverWait(self.driver.instance, 2).until(
+            EC.visibility_of_element_located((
+                MobileBy.XPATH, "//button[@name='submit']"
+            ))
+        )
+        self.fill_user_data(textbox_username, textbox_password)
+        button_login.click()
+
+        assert self.verify_user_login()
 
     def switch_context(self, new_context):
 
         c_context = self.driver.instance.mobile.contexts
+        self.driver.instance.switch_to.context(new_context)
 
-        print(c_context)
-        print("__BEFORE__")
-        print(self.driver.instance.mobile.context)
-        self.driver.instance.switch_to.context("WEBVIEW_chrome")
-        print("__after__")
-        print(self.driver.instance.mobile.context)
+    def fill_user_data(self, username, password):
+        username.send_keys(login_user)
+        password.send_keys(login_password)
 
-       # print(self.driver.instance.context + ": Before ")
-       #  con = self.driver.instance.contexts
-        # print(con)
-       # self.driver.instance.context("WEBVIEW_chrome")
-        #print(self.driver.instance.context + ": Before ")
+    def verify_user_login(self):
+        self.switch_context("NATIVE_APP")
 
+        button_logged = WebDriverWait(self.driver.instance, 15).until(
+            EC.visibility_of_element_located((
+                MobileBy.XPATH, "//android.widget.Button"
+            ))
+        )
+        return True
